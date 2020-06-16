d6c1cb49ce8069097060348a4c65f258a763713c
Trung Pham <phamanhtrung@gmail.com>
2020-06-16 17:57:34 -0500

    test user input and submit button successfully

--- get-it-done-app/js/app.js
+++ get-it-done-app/js/app.js
@@ -1,3 +1,14 @@
 $(() => {
   console.log("Connected to js file!");
+
+  // const inputSearch = $("#searchTerm");
+  const inputSearch = document.getElementById("searchTerm");
+  const button = document.getElementById("searchButton");
+
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    const searchTerm = inputSearch.value;
+
+    console.log(searchTerm);
+  });
 });
