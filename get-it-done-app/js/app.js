52cb2a7dc08178169d3f02e5e7877318ab3e3bc5
Trung Pham <phamanhtrung@gmail.com>
2020-06-16 20:17:18 -0500

    test out FavQs api

--- get-it-done-app/js/app.js
+++ get-it-done-app/js/app.js
@@ -1,3 +1,22 @@
+// QuoteGarden URL
+const randomQuoteURL =
+  "https://quote-garden.herokuapp.com/api/v2/quotes/random";
+// const allQuoteURL =
+//   "https://quote-garden.herokuapp.com/api/v2/quotes?page=1&limit=10";
+// const quoteURLByAuthor =
+//   "https://quote-garden.herokuapp.com/api/v2/authors/:Jenny-McCarthy?page=1&limit=10";
+// const quoteURLByGenre =
+//   "https://quote-garden.herokuapp.com/api/v2/genre/:marriage";
+
+// FavQs URL
+const baseURL = "https://favqs.com/api/";
+const apiKey = "57466b162e44774694d5770fdefbf733";
+const quoteOfTheDay = "https://favqs.com/api/qotd";
+// const quoteContaining = "https://favqs.com/api/quotes/?filter=funny";
+// const quoteTypeAhead = "https://favqs.com/api/typeahead";
+// const quoteByAuthor =
+//   "GET https://favqs.com/api/quotes/?filter=Mark+Twain&type=author";
+
 $(() => {
   console.log("Connected to js file!");
 
@@ -13,14 +32,20 @@ $(() => {
   });
 
   $.ajax({
-    url: "https://quote-garden.herokuapp.com/api/v2/quotes/random",
+    type: "GET",
+    url: quoteByAuthor,
+    token: "57466b162e44774694d5770fdefbf733",
   }).then((quoteData) => {
     console.log("Return quote data success!");
+    // $(".container").append(`
+    //   <p> "${quoteData.quote.quoteText}" </p>
+    //   <p>- ${quoteData.quote.quoteAuthor} </p>
+    // `);
     $(".container").append(`
-      <p> "${quoteData.quote.quoteText}" </p>
-      <p>- ${quoteData.quote.quoteAuthor} </p>
+      <p> "${quoteData.quote.body}" </p>
+      <p>- ${quoteData.quote.author} </p>
     `);
-    // console.log(quoteData.quote);
+    console.log(quoteData.quote);
     // console.log(quoteData.quote.quoteText);
     // console.log(quoteData.quote.quoteAuthor);
   });
